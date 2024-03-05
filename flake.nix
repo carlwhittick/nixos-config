fb9ce84b6cd2dab8280c51ffd6d1be422fed3105
Carl Whittick <carlwhittick@gmail.com>
2024-03-05 23:03:02 +0000

    Updated configs

--- flake.nix
+++ flake.nix
@@ -7,13 +7,10 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
+
       ];
     };
   };
