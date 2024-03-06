13f99533b52fb25492eb47b9f8ab2adab1b44968
Carl Whittick <carlwhittick@gmail.com>
2024-03-06 01:21:19 +0000

    Revert "Updated configs"
    This reverts commit fb9ce84b6cd2dab8280c51ffd6d1be422fed3105.

--- configuration.nix
+++ configuration.nix
@@ -72,7 +72,7 @@ in
     "v4l2loopback"
   ];
 
-  networking.hostName = "my-nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
--- flake.nix
+++ flake.nix
@@ -7,10 +7,13 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
       ];
     };
   };
